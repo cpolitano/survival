db57fc9e95dad7d5d91e365f73368a5a2c498848
stbarrientos <stbarrientos@gmail.com>
2014-10-06 15:40:05 -0400

    finished usa_today api

--- app/assets/javascripts/survival_calc.js
+++ app/assets/javascripts/survival_calc.js
@@ -1,19 +1,24 @@
-// var apiKey = "hequvx2dwyfntbhae7b769hr";
-// var zipcode = "Great Falls";
-// $.ajax({
-// 	url: "http://api.usatoday.com/open/census/loc?keypat=" + zipcode + "&keyname=placename&sumlevid=4,6&api_key=" + apiKey,
-// 	method: "GET",
-// 	dataType: "jsonp",
-// 	success: function(data){
-// 		console.log(data);
-// 	}
-// });
+var apiKey = "hequvx2dwyfntbhae7b769hr";
+var googlePlacesKey = "AIzaSyDae3V_0lLmnd2awsbZFT55GMt7fgAaOBI";
 
-// $.ajax({
-// 	url: "http://api.usatoday.com/open/census/loc?keypat=New York&keyname=placename&sumlevid=3&api_key=" + apiKey,
-// 	method: "GET",
-// 	dataType: "jsonp",
-// 	success: function(data){
-// 		console.log(data);
-// 	}
-// });
+$("#search").on('submit', function(event){
+	event.preventDefault();
+	city = $("#city").val();
+	state = $("#state").val();
+	findCity(city, state);
+})
+
+function findCity(city, state){
+	$.ajax({
+		url: "http://api.usatoday.com/open/census/loc?keypat=" + city + "&keyname=placename&sumlevid=4,6&api_key=" + apiKey,
+		method: "GET",
+		dataType: "jsonp",
+		success: function(data){
+			for (var i = 0; i < data.response.length; i ++){
+				if (data.response[i].StatePostal === state){
+					$("body").append("Population: " + data.response[i].Pop + "<br>Latitude: " + data.response[i].Lat + ", Longitude: " + data.response[i].Long)
+				}
+			}
+		}
+	});
+}
