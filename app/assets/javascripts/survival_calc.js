4ba0410dd23b3c85ccaed028aabbb338548555cd
stbarrientos <stbarrientos@gmail.com>
2014-10-07 09:37:07 -0400

    attempting to fix merge conflict

--- app/assets/javascripts/survival_calc.js
+++ app/assets/javascripts/survival_calc.js
@@ -17,8 +17,39 @@ function findCity(city, state){
 			for (var i = 0; i < data.response.length; i ++){
 				if (data.response[i].StatePostal === state){
 					$("body").append("Population: " + data.response[i].Pop + "<br>Latitude: " + data.response[i].Lat + ", Longitude: " + data.response[i].Long)
+					console.log(initialize(data.response[i].Lat, data.response[i].Long, "guns"));
 				}
 			}
 		}
 	});
 }
+
+var map;
+var infowindow;
+
+function initialize(lat,lng, search) {
+  var currentLocation = new google.maps.LatLng(lat, lng);
+
+  map = document.getElementById('asdf')
+
+  var request = {
+    location: currentLocation,
+    radius: "1",
+    types: ["store"],
+    query: search
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results.length);
+    for (var i = 0; i < results.length; i ++){
+    	console.log(results[i]);
+    }
+  }
+}
+
+// google.maps.event.addDomListener(window, 'load', initialize(0,0,"guns"));
