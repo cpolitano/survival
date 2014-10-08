e0774778f13ff93ee586a1cb7e927447d13216d6
stbarrientos <stbarrientos@gmail.com>
2014-10-08 15:22:59 -0400

    db all set up and ready to collect

--- app/assets/javascripts/survival_calc.js
+++ app/assets/javascripts/survival_calc.js
@@ -1,12 +1,11 @@
 var apiKey = "hequvx2dwyfntbhae7b769hr";
 var googlePlacesKey = "AIzaSyDae3V_0lLmnd2awsbZFT55GMt7fgAaOBI";
 var map;
-var infowindow;
 var count;
-var query;
-var population;
-var returnObj = {};
+var name;
+var population = 0;
 var stores = 0;
+var score = 0;
 var runCount = 0;
 var survivalRating = 0;
 
@@ -14,10 +13,37 @@ $("#search").on('submit', function(event){
 	event.preventDefault();
 	city = $("#city").val();
 	state = $("select option:selected").val();
-	findCity(city, state);
+	checkDB(city,state)
 	$("form").hide();
 	$(".search-hidden").show();
 });
+
+function checkDB(city, state){
+	$.ajax({
+		url: "/zombies",
+		dataType: "json",
+		method: "GET",
+		success: function(data){
+			console.log(data[0])
+			var needAjax = true;
+			for (var i = 0; i < data.length; i ++){
+				if (data[i].name === city.toLowerCase() + ", " + state.toLowerCase()){
+					needAjax = false;
+					population = data[i].population;
+					score = data[i].score;
+					stores = data[i].stores;
+				}
+			}
+			if (needAjax){
+				findCity(city,state)
+			} else {
+				$("#results").append("<p>Population: " + population + "</p>");
+    		$("#results").append("<p>Useful Stores Nearby: " + stores + "</p>");
+				$("#results").append("<p>Chance Of Survival: " + score + "%</p>");
+			}
+		}
+	})
+}
 
 function findCity(city, state){
 	$.ajax({
@@ -28,6 +54,7 @@ function findCity(city, state){
 			if (data.response.length > 0){
 				for (var i = 0; i < data.response.length; i ++){
 					if (data.response[i].StatePostal === state){
+						name = city.toLowerCase() + ", " + state.toLowerCase();
 						population = parseInt(data.response[i].Pop);
 						initialize(data.response[i].Lat, data.response[i].Long, "guns");
 						initialize(data.response[i].Lat, data.response[i].Long, "grocery");
@@ -51,21 +78,25 @@ function initialize(lat,lng, search) {
     types: ["store"],
     keyword: search
   };
-  infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
-    returnObj["value"] = results.length;
     stores += results.length;
     runCount++;
     if (runCount === 2){
     	$("#results").append("<p>Population: " + population + "</p>");
     	$("#results").append("<p>Useful Stores Nearby: " + stores + "</p>");
-    	var score = algorithm(population, stores);
+    	score = algorithm(population, stores);
     	$("#results").append("<p>Chance Of Survival: " + score + "%</p>");
+    	$.ajax({
+    		url: "/cities",
+    		method: "POST",
+    		dataType: "json",
+    		data: {city: {population: population, stores: stores, name: name, score: score}},
+    	})
     }
   } else {
   	runCount++;
@@ -78,7 +109,6 @@ function callback(results, status) {
 function algorithm(population, stores){
 	if ( population > 1000000 ) {
 		survivalRating += 0 + (stores/6);
-		console.log(survivalRating);
 	}
 	else if ( population > 250000 && population < 1000000 ) {
 		survivalRating += 10 + (stores/5);
