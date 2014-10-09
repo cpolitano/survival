a8e54ee58ff4789ca7151cc6a1a89c6baa8b0959
jakecatt <jakecatt@gmail.com>
2014-10-09 15:10:25 -0400

    mobile responsive

--- app/assets/javascripts/survival_calc.js
+++ app/assets/javascripts/survival_calc.js
@@ -114,11 +114,13 @@ function callback(results, status) {
     runCount++;
     if (runCount === 2){
     	runCount = 0;
-    	$("#results").html("<p>" + htmlName + "</p>");
-    	$("#results").append("<p>Your Chance Of Survival: " + score + "%</p>");
-    	$("#results").append("<p>Based on Population: " + population + "</p>");
+
+    	$("#results").html("<h2>" + htmlName + "</h2>");
+    	$("#results").append("<div id='survival-chance'><h3>your chance of survival: <span>" + score + "%</span></h3></div>");
+    	$("#results").append("<h4>population: <span>" + population + "</span></h4>");
     	score = algorithm(population, stores);
-    	$("#results").append("<p>+ Useful Stores Nearby: " + stores + "</p>");
+    	$("#results").append("<h4> useful stores nearby: <span>" + stores + "</span></h4>");
+
     	$.ajax({
     		url: "/cities",
     		method: "POST",
